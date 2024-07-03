967ed4bcf08dd2b31b59d72568817338147f8a86
Olivier Mars <olivier@omrs.fr>
2024-07-03 23:30:56 +0200

    add Web server settings

--- conf/config.sample.php
+++ conf/config.sample.php
@@ -13,3 +13,6 @@ define('DB_NAME', "database");
 /* E-mail server settings */
 define('EMAIL_DOMAIN', "domain.tld");
 define('DOVECOT_API_URL', "https://mail.domain.tld/doveadm/v1");
+
+/* Web server settings*/
+define('ROOT', str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
